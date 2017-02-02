4c41186b08579e02e8e4f403882be685333f0b03
Leighton Wallace <leighton@lever.co>
2017-02-01 17:28:25 -0800

    Add some spacing to post install message

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,3 +1,5 @@
 #! /usr/bin/env node
-console.log('Install hooks by running the following command from your repo root:');
+console.log();
+console.log('Install affiance hooks by running the following command from your Git repo root:');
 console.log('  ./node_modules/.bin/affiance install');
+console.log();

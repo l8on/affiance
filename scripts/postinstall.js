f99f4e03b64609bc45b33b0dfb76857b0de4c40e
Leighton Wallace <leighton@lever.co>
2017-04-23 22:08:22 -0700

    Add signature command to post install message

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,3 +3,5 @@ console.log();
 console.log('Install affiance hooks by running the following command from your Git repo root:');
 console.log('  ./node_modules/.bin/affiance install');
 console.log();
+console.log('If you are using the default configuration or otherwise have signatures enabled, and you trust this installation run:');
+console.log('  ./node_modules/.bin/affiance sign');

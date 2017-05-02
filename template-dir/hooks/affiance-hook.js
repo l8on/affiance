10512c0fd8d9afa711cc2af7a5ab8acb7f76f26c
Leighton Wallace <leighton@lever.co>
2017-05-02 00:04:03 -0700

    Improve performance by running commands simultaneously
    With everything running serially and synchronously, affiance is slow.
    This was somewhat expected, but in order to get any real usage,
    something must be done!
    This moves affiance in the direction of allowing hooks to return a
    promise that will resolve with the same type of result as before. To
    allow for simple hooks that return synchronously, `HookBase.wrapRun`
    will also wrap the return value in a promise if necessary.
    Finally, since we are now using the `Promise` class, it is time to start
    using `es6` features in `affiance`.
    
    * Add `use strict` to the top of files with es6 syntax
    * Add `spawnConcurrentCommandsOnApplicableFiles` function that will help
      make shell command based hooks easier to make more performant
    * Use `spawnConcurrentCommandsOnApplicableFiles` function in `EsLint`
      hook and `CoffeeLint` hooks for better performance
    * Configure `EsLint` to parallelize
    * Add `es6` mode to `.eslintrc.yml`

--- template-dir/hooks/affiance-hook.js
+++ template-dir/hooks/affiance-hook.js
@@ -1,3 +1,4 @@
+"use strict";
 // Get the type of hook from the arguments.
 var hookType = process.argv[2];
 // Capture original args sent to hook script
@@ -32,14 +33,52 @@ try {
     }
 }
 
+function handleErrorAndExit(e) {
+  console.log('in handleErrorAndExit', e);
+  // Affiance specific errors all have an `affianceName` property.
+  // If undefined, will use the default condition.
+  switch(e.affianceName) {
+    case Affiance.error.ConfigurationError:
+    case Affiance.error.ConfigurationSignatureChanged:
+      console.log(e.message);
+      process.exit(77); // EX_CONFIG
+
+    case Affiance.error.HookSetupFailed:
+    case Affiance.error.HookCleanupFailed:
+      console.log(e.message);
+      process.exit(74); // EX_IOERR
+
+    case Affiance.error.HookLoadError:
+    case Affiance.error.InvalidHookDefinition:
+      console.log(e.message);
+      if (e.stack) { console.log(e.stack); }
+      process.exit(78); // EX_CONFIG
+
+    case Affiance.error.InvalidGitRepo:
+      console.log(e.message);
+      process.exit(64); // EX_USAGE
+
+    default:
+      console.log(e.message);
+      if (e.stack) { console.log(e.stack); }
+      process.exit(70); // EX_SOFTWARE
+  }
+};
+
+// Capture unhandled Rejections as well
+process.on('unhandledRejection', (e, p) => {
+  console.log('~~~~ process unhandledRejection', p);
+  handleErrorAndExit(e);
+});
+
 // Run the requested hook based on the repo's config.
 try {
     var logger = new Affiance.Logger({level: process.env.LOG_LEVEL});
 
     // Ensure master hook is up to date
     var installer = new Affiance.Installer(logger);
-    // TODO figure out how to get this to actually work! Currently hangs when spawning.
 
+    // TODO figure out how to get this to actually work! Currently hangs when spawning.
     if (installer.update()) {
         // If the installation was updated, re-run the current command.
         var childProcess = require('child_process');
@@ -49,7 +88,6 @@ try {
         process.exit(spawnResult.status);
     }
 
-
     var config  = Affiance.configLoader.loadRepoConfig();
     var hookContext = Affiance.HookContext.createContext(hookType, config, originalArgs, process.stdin);
     config.applyEnvironment(hookContext, process.env);
@@ -57,41 +95,37 @@ try {
     var printer = new Affiance.Printer(config, logger, hookContext);
     var hookRunner = new Affiance.HookRunner(config, logger, hookContext, printer);
 
-    var status = hookRunner.run();
-    process.exit(status ? 0 : 65); // EX_DATAERR
+    process.on('exit', function(code) { console.log('~~~~~process exited', code)});
+
+    // catches ctrl+c event
+    process.on('SIGINT', function() { console.log('~~~~~process siginted'); process.exit(); });
+
+    //catches uncaught exceptions
+    process.on('uncaughtException', function(e) { console.log('~~~~~process uncaughtExceptioned', e)});
+    process.on('unhandledRejection', function(reason, p) { console.log('~~~~~process unhandledRejection', reason, p)});
+    process.on('rejectionHandled', function() { console.log('~~~~~process rejectionHandled')});
+
+    console.log('in affiance-hook.js before statusPromise');
+    var statusPromise = hookRunner.run();
+    console.log('in affiance-hook.js after statusPromise', statusPromise);
+
+    statusPromise.then(function(result) {
+      console.log('in affiance-hook.js statusPromise#then', result);
+      // We finished, use the result
+      process.exit(result ? 0 : 65); // EX_DATAERR
+
+    }).catch(function(result) {
+      console.log('in affiance-hook.js statusPromise#catch', result);
+      if (e.stack) { console.log(result.stack); }
+      // If an affiance error was thrown, rethrow error so it is caught in block below.
+      if (typeof result === 'object') { throw result; }
+
+      // Something went wrong!
+      console.log('in affiance-hook.js statusPromise#catch', result);
+      process.exit(65); // EX_DATAERR
+    });
 
 } catch (e) {
-    // Affiance specific errors all have an `affianceName` property.
-    // If undefined, will use the default condition.
-    switch(e.affianceName) {
-      case Affiance.error.ConfigurationError:
-      case Affiance.error.ConfigurationSignatureChanged:
-        console.log(e.message);
-        process.exit(77); // EX_CONFIG
-
-      case Affiance.error.HookSetupFailed:
-      case Affiance.error.HookCleanupFailed:
-        console.log(e.message);
-        process.exit(74); // EX_IOERR
-
-      case Affiance.error.HookLoadError:
-      case Affiance.error.InvalidHookDefinition:
-        console.log(e.message);
-        if (e.stack) { console.log(e.stack); }
-        process.exit(78); // EX_CONFIG
-
-      case Affiance.error.InvalidGitRepo:
-        console.log(e.message);
-        process.exit(64); // EX_USAGE
-
-      default:
-        console.log(e.message);
-        if (e.stack) { console.log(e.stack); }
-        process.exit(70); // EX_SOFTWARE
-    }
+  console.log('in affiance-hook.js catch block');
+  handleErrorAndExit(e);
 }
-
-
-
-
-
